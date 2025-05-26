51f896d24f6d120ec302fd5e8b5783f07c2116c1
dheric <dhericdrake@gmail.com>
2025-05-26 12:10:52 -0300

    FIX: corrigindo erros do front

--- src/hooks/useGetUser.js
+++ src/hooks/useGetUser.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { getToken } from '../utils/localStorageUtil';
 import { toast } from 'sonner';
 import { jwtDecode } from 'jwt-decode';
-import axios from 'axios';
 import api from '../services/api';
 
 export const useGetUser = () => {
@@ -12,6 +11,7 @@ export const useGetUser = () => {
     useEffect(() => {
         const token = getToken();
 
+        // Se não houver token, apenas termina a execução sem fazer requisição
         if (!token) {
             setLoading(false);
             return;
@@ -26,13 +26,16 @@ export const useGetUser = () => {
             return;
         }
 
+        // Função para buscar dados do usuário
         const fetchUser = async () => {
             setLoading(true);
             try {
                 const res = await api.get('/usuarios/me', {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                }});
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    }
+                });
+                // Salva os dados do usuário e a role decodificada
                 setUser({ ...res.data, role: decoded.role });
             } catch (error) {
                 toast.error('Falha ao carregar o usuário');
@@ -42,7 +45,7 @@ export const useGetUser = () => {
         };
 
         fetchUser();
-    }, []);	
+    }, []); // Dependência vazia significa que esse efeito roda uma vez no início
 
     return { user, loading };
-}
+};
